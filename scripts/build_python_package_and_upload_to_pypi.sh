28e0c39716fdf4bc6590b6a014231f0a26193d4d
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-01-18 14:55:49 +0100

    scripts to release PyPi package and create QGIS plugin zip file

---
+++ scripts/build_python_package_and_upload_to_pypi.sh
@@ -0,0 +1,22 @@
+PWD=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PYTHON_PACKAGE_DIR=python-package
+PACKAGE_DIR=$DIR/../$PYTHON_PACKAGE_DIR
+DEST_BUILD="$PACKAGE_DIR/build"
+
+#render rst file with readme for package - needs Pandoc
+pandoc --to=rst -o $PACKAGE_DIR/README.rst README.md
+
+# remove build dir to avoid having multiple versions there
+rm -rf DEST_BUILD
+
+# build python package and upload to PyPi - authentication for PyPi needs to be set
+cd $PACKAGE_DIR
+python3 -m build
+twine check dist/*
+python3 -m twine upload dist/*
+
+# copy whl file to QGIS plugin directory to keep python package and qgis plugin in sync
+QGIS_DEP_DIR=$PACKAGE_DIR/../qgis-plugin/limburg_flood_impact_plugin/deps
+rm $QGIS_DEP_DIR/*
+cp dist/limburg_flood_impact-*.whl $QGIS_DEP_DIR
---
+++ scripts/create_qgis_plugin_package.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+########################################################################################################################
+# Create ZIP package of the plugin ready for upload to plugin repository
+########################################################################################################################
+
+set -u # Exit if we try to use an uninitialised variable
+set -e # Return early if any command returns a non-0 exit status
+
+echo "CREATE PLUGIN"
+
+command -v zip >/dev/null 2>&1 || { echo "I require zip but it's not installed.  Aborting." >&2; exit 1; }
+
+PWD=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PLUGIN=limburg_flood_impact_plugin
+SRC=$DIR/../qgis-plugin/$PLUGIN
+DEST="$DIR/../qgis-plugin/build"
+DEST_BUILD=$DEST/$PLUGIN
+
+if [ ! -d "$SRC" ]; then
+  echo "Missing directory $SRC"
+  exit 1
+fi
+
+rm -rf $DEST_BUILD
+mkdir -p $DEST_BUILD
+
+cp -R $SRC/* $DEST_BUILD/
+find $DEST_BUILD -type l -exec unlink {} \;
+
+find $DEST_BUILD -name \*.pyc -delete
+find $DEST_BUILD -name __pycache__ -delete
+
+cd $DEST_BUILD/..
+zip -r $DEST/$PLUGIN.zip $PLUGIN --exclude *.idea*
+
+echo "$DEST/$PLUGIN.zip created"
+
+cd $PWD
