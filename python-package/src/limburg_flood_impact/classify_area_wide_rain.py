d60f4116ece24800a50811023a2c7edc8b0d8d55
Jan Caha <jan.caha@outlook.com>
2023-02-02 09:40:13 +0100

    if qgis_feedback is passed, check for user interuption and cancel the calculation accordingly

--- python-package/src/limburg_flood_impact/classify_area_wide_rain.py
+++ python-package/src/limburg_flood_impact/classify_area_wide_rain.py
@@ -18,7 +18,8 @@ def classify_water_height(buildings_ds: gdal.Dataset,
                           t25: gdal.Dataset,
                           t100: gdal.Dataset,
                           field_name: str = "gebiedsbreed",
-                          callback_function: Callable[[float], None] = None):
+                          callback_function: Callable[[float], None] = None,
+                          qgis_feedback=None):
 
     buildings_layer: ogr.Layer = buildings_ds.GetLayer()
 
@@ -57,6 +58,10 @@ def classify_water_height(buildings_ds: gdal.Dataset,
     feature: ogr.Feature
     i = 0
     for feature in buildings_layer:
+
+        if qgis_feedback is not None:
+            if qgis_feedback.isCanceled():
+                break
 
         delete_all_features_from_layer(memory_layer)
 
@@ -135,7 +140,8 @@ def classify_area_wide_rain(buildings_path: Path,
                             t10: Path,
                             t25: Path,
                             t100: Path,
-                            callback_function: Callable[[float], None] = None):
+                            callback_function: Callable[[float], None] = None,
+                            qgis_feedback=None):
 
     buildings_ds: ogr.DataSource = ogr.Open(buildings_path.as_posix(), True)
 
@@ -144,12 +150,40 @@ def classify_area_wide_rain(buildings_path: Path,
     t100_ds: gdal.Dataset = gdal.Open(t100.as_posix())
 
     t10_masked_including_puddles = flood_mask(t10_ds, only_water_height_above=0.02)
+
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
+
     t25_masked_including_puddles = flood_mask(t25_ds, only_water_height_above=0.02)
+
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
+
     t100_masked_including_puddles = flood_mask(t100_ds, only_water_height_above=0.02)
 
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
+
     t10_masked = flood_mask(t10_ds, only_water_height_above=0.02, minimal_area_of_water_pond=200)
+
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
+
     t25_masked = flood_mask(t25_ds, only_water_height_above=0.02, minimal_area_of_water_pond=200)
+
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
+
     t100_masked = flood_mask(t100_ds, only_water_height_above=0.02, minimal_area_of_water_pond=200)
+
+    if qgis_feedback:
+        if qgis_feedback.isCanceled():
+            return
 
     classify_water_height(buildings_ds,
                           t10_masked_including_puddles,
@@ -158,7 +192,8 @@ def classify_area_wide_rain(buildings_path: Path,
                           t10_masked,
                           t25_masked,
                           t100_masked,
-                          callback_function=callback_function)
+                          callback_function=callback_function,
+                          qgis_feedback=qgis_feedback)
 
     buildings_ds = None
     t10_ds = None
