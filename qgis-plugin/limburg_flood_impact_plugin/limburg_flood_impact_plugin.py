990645247fc0c06f1a60c5ec3cd2530584507ddf
Jan Caha <jan.caha@outlook.com>
2023-06-16 15:11:48 +0200

    add debug config

--- qgis-plugin/limburg_flood_impact_plugin/limburg_flood_impact_plugin.py
+++ qgis-plugin/limburg_flood_impact_plugin/limburg_flood_impact_plugin.py
@@ -9,10 +9,17 @@ try:
 except ModuleNotFoundError:
     # if required modules are not available on system, let's use the versions that we package
     this_dir = os.path.dirname(os.path.realpath(__file__))
-    deps_dir = os.path.join(this_dir, 'deps')
+    deps_dir = os.path.join(this_dir, "deps")
     if os.path.exists(deps_dir):
         for f in os.listdir(os.path.join(deps_dir)):
             sys.path.append(os.path.join(deps_dir, f))
+
+    # this configuration is to be used in debug mode, to develop the plugin and
+    # to allow debugging in the python package code
+    # from pathlib import Path
+    # p = Path(__file__).parent.parent.parent / "python-package" / "src"
+    # sys.path.append(p.as_posix())
+
 
 from .processing.limburg_flood_impact_provider import LimburgFloodImpactProvider
 
