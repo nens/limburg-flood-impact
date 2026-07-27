96f2ca35f0fd8afc8fcec6108b9817c2ca9cd3fd
Ben van Basten <ben.vanbasten@nelen-schuurmans.nl>
2026-07-27 15:15:37 +0200

    Fixed update issue

--- qgis-plugin/limburg_flood_impact_plugin/limburg_flood_impact_plugin.py
+++ qgis-plugin/limburg_flood_impact_plugin/limburg_flood_impact_plugin.py
@@ -6,13 +6,21 @@ from qgis.gui import QgisInterface
 
 try:
     import limburg_flood_impact
+    if limburg_flood_impact.get_version() != "0.9.0":
+        raise ModuleNotFoundError()
+
 except ModuleNotFoundError:
+    # Remove any stale cached modules so re-import picks up the bundled version
+    stale = [key for key in sys.modules if key == "limburg_flood_impact" or key.startswith("limburg_flood_impact.")]
+    for key in stale:
+        del sys.modules[key]
+
     # if required modules are not available on system, let's use the versions that we package
     this_dir = os.path.dirname(os.path.realpath(__file__))
     deps_dir = os.path.join(this_dir, "deps")
     if os.path.exists(deps_dir):
         for f in os.listdir(os.path.join(deps_dir)):
-            sys.path.append(os.path.join(deps_dir, f))
+            sys.path.insert(0, os.path.join(deps_dir, f))
 
     # this configuration is to be used in debug mode, to develop the plugin and
     # to allow debugging in the python package code
@@ -20,6 +28,8 @@ except ModuleNotFoundError:
     # p = Path(__file__).parent.parent.parent / "python-package" / "src"
     # sys.path.append(p.as_posix())
 
+
+import limburg_flood_impact
 
 from .processing.limburg_flood_impact_provider import LimburgFloodImpactProvider
 
